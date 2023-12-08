4d33d2c0a7e11ea42df9ddd5b245e930a26e7b0b
KamalSeb <kamalsebati@outlook.com>
2023-12-08 03:59:06 -0500

    Ajouter le squelette Spring Boot

--- src/main/java/com/kamal/calculator/CalculatorController.java
+++ src/main/java/com/kamal/calculator/CalculatorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class CalculatorController {
 @Autowired
-private Calculator calculator;
+private calculator calculator;
 @RequestMapping("/sum")
 String sum(@RequestParam("a") Integer a,
 @RequestParam("b") Integer b) {
