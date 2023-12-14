f29c14feb4b11d7535a7d83870dbb42bead7e0db
KamalSeb <kamalsebati@outlook.com>
2023-12-14 15:37:21 -0500

    Ajouter la logique de l’addition, controller et test unitaire

--- src/test/java/com/kamal/calculator/CalculatorTest.java
+++ src/test/java/com/kamal/calculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.kamal.calculator;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
 private Calculator calculator = new Calculator();
 @Test
